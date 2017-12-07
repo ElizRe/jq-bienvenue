6e7092b842ebdfdbc12e9277fb58cecc2caed47b
ElizRe <elizreed@laposte.net>
2017-12-07 16:28:59 +0100

    new files

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('have fun !');
+
+$('#button').click(function(){
+
+	var first = $("#first_name").val();
+	var last = $("#last_name").val();
+	var city = $("#city").val();
+	
+	$('#username').html (first +" "+" "+last + " " + city);
+
+})
